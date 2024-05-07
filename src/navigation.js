916e9cbbcffb819f10bb44f4eda9e7367aeb92fb
vikramaruchamy <aruchamyvikram@gmail.com>
2024-05-07 18:17:20 +0530

    Initial Version commit

---
+++ src/navigation.js
@@ -0,0 +1,81 @@
+import { getPermalink, getBlogPermalink,getHomePermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'Home',
+      href: getHomePermalink(),
+      
+    },
+    {
+        text: 'Pricing',
+        href: getPermalink('/pricing'),
+      },
+    {
+      text: 'Pages',
+      links: [
+        
+        {
+          text: 'Terms',
+          href: getPermalink('/terms-of-services'),
+        },
+        {
+          text: 'Privacy policy',
+          href: getPermalink('/privacy-policy'),
+        },
+      ],
+    },
+    
+    {
+      text: 'Blog',
+      href: getBlogPermalink(),
+      
+    },
+   
+  ],
+  actions: [{ text: 'Install free add on', href: 'https://workspace.google.com/marketplace/app/docs_to_markdown_pro/483386994804?utm_source=docstomarkdown.pro&utm_medium=main_menu_install_free_add-on&utm_id=app_home', target: '_blank' }],
+};
+
+export const footerData = {
+  links: [
+    {
+      title: 'Product',
+      links: [
+        { text: 'Features', href: '#' },
+        { text: 'Security', href: '#' },
+        { text: 'Team', href: '#' },
+        { text: 'Enterprise', href: '#' },
+        { text: 'Customer stories', href: '#' },
+        { text: 'Pricing', href: '#' },
+        { text: 'Resources', href: '#' },
+      ],
+    },
+    
+    {
+      title: 'Support',
+      links: [
+        { text: 'Docs', href: '#' },
+        { text: 'Community Forum', href: '#' },
+        { text: 'Professional Services', href: '#' },
+        { text: 'Skills', href: '#' },
+        { text: 'Status', href: '#' },
+      ],
+    },
+    
+  ],
+  secondaryLinks: [
+    { text: 'Terms', href: getPermalink('/terms-of-services') },
+    { text: 'Privacy Policy', href: getPermalink('/privacy-policy') },
+  ],
+  socialLinks: [
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+  ],
+  footNote: `
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+  `,
+};
