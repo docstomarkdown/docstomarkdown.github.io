102887485ea82c3bb7f57cdf614a9dfd5bbf8a9e
vikramaruchamy <aruchamyvikram@gmail.com>
2024-05-18 11:52:59 +0530

    Included Sentry error logging

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,96 +1,100 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-import { defineConfig, squooshImageService } from 'astro/config';
-
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown';
-import icon from 'astro-icon';
-
-
-import astrowind from './vendor/integration';
-
+import path from "path";
+import { fileURLToPath } from "url";
+import { defineConfig, squooshImageService } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+import icon from "astro-icon";
+import astrowind from "./vendor/integration";
 import {
   readingTimeRemarkPlugin,
   responsiveTablesRehypePlugin,
   lazyImagesRehypePlugin,
-} from './src/utils/frontmatter.mjs';
-
+} from "./src/utils/frontmatter.mjs";
+import sentry from "@sentry/astro";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 const hasExternalScripts = false;
 const whenExternalScripts = (items = []) =>
-  hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
+  hasExternalScripts
+    ? Array.isArray(items)
+      ? items.map((item) => item())
+      : [items()]
+    : [];
 
+// https://astro.build/config
 export default defineConfig({
-  output: 'static',
-
+  output: "static",
   integrations: [
+    sentry({
+      dsn: "https://3502dc747e79fa21814b87fdbc9bad28@o4507275942952960.ingest.us.sentry.io/4507275945443328",
+      sourceMapsUploadOptions: {
+        project: "docs-to-markdown-pro",
+        authToken: "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",
+      },
+    }),
     tailwind({
       applyBaseStyles: false,
     }),
     sitemap({
-        filter: (page) => !page.startsWith('https://www.docstomarkdown.pro/tag/') &&
-        !page.startsWith('https://www.docstomarkdown.pro/blog/'),
-      }),
+      filter: (page) =>
+        !page.startsWith("https://www.docstomarkdown.pro/tag/") &&
+        !page.startsWith("https://www.docstomarkdown.pro/blog/"),
+    }),
     mdx(),
     icon({
       include: {
-        tabler: ['*'],
-        'flat-color-icons': [
-          'template',
-          'gallery',
-          'approval',
-          'document',
-          'advertising',
-          'currency-exchange',
-          'voice-presentation',
-          'business-contact',
-          'database',
+        tabler: ["*"],
+        "flat-color-icons": [
+          "template",
+          "gallery",
+          "approval",
+          "document",
+          "advertising",
+          "currency-exchange",
+          "voice-presentation",
+          "business-contact",
+          "database",
         ],
       },
     }),
-
     ...whenExternalScripts(() =>
       partytown({
-        config: { forward: ['dataLayer.push'] },
+        config: {
+          forward: ["dataLayer.push"],
+        },
       })
     ),
-
     /*compress({
-      CSS: true,
-      HTML: {
-        'html-minifier-terser': {
-          removeAttributeQuotes: false,
-        },
+    CSS: true,
+    HTML: {
+      'html-minifier-terser': {
+        removeAttributeQuotes: false,
       },
-      Image: false,
-      JavaScript: true,
-      SVG: false,
-      Logger: 1,
-    }),*/
+    },
+    Image: false,
+    JavaScript: true,
+    SVG: false,
+    Logger: 1,
+  }),*/
 
     astrowind({
-      config: './src/config.yaml',
+      config: "./src/config.yaml",
     }),
+    sentry(),
   ],
-
   image: {
     service: squooshImageService(),
-    domains: ['cdn.pixabay.com'],
+    domains: ["cdn.pixabay.com"],
   },
-
   markdown: {
     remarkPlugins: [readingTimeRemarkPlugin],
     rehypePlugins: [responsiveTablesRehypePlugin, lazyImagesRehypePlugin],
   },
-
   vite: {
     resolve: {
       alias: {
-        '~': path.resolve(__dirname, './src'),
+        "~": path.resolve(__dirname, "./src"),
       },
     },
   },
